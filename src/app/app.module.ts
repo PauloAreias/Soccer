8056c84c461bdf58f1230ef4dc30811dbc895861
Areias <paulocarlaoareias@gmail.com>
2019-05-06 20:24:45 +0100

    Add API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LigasComponent } from './ligas/ligas.component';
+import { LigasService } from "./ligas/ligas.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { LigasComponent } from './ligas/ligas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [LigasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ligas/ligas.component.ts
+++ src/app/ligas/ligas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LigasService } from './ligas.service';
 
 @Component({
   selector: 'app-ligas',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LigasComponent implements OnInit {
 
-  constructor() { }
+  ligas: {};
+  liga : String;
+  name: String;
+
+  constructor(private ligaService: LigasService) { }
 
   ngOnInit() {
+
+    this.ligaService.getCurrentLiga(this.liga);
+
+    this.liga= "PPL";
+
+    this.name ="1ª Liga"
+
+    this.getCurrentLiga(this.liga)
+
   }
 
+  SelectLiga(liga, name){
+    this.name= name;
+    this.liga=liga;
+    this.getCurrentLiga(liga);
+  }
+
+  getCurrentLiga(liga){
+
+    this.ligaService.getCurrentLiga(liga).subscribe(ligas => this.ligas = ligas);
+  }
 }
